596ef73b674dc247004fe808d99153ac70627c01
blamet <arturbaldoramos@gmail.com>
2022-04-12 13:23:36 -0300

    changed imput password

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
       <header className="App-header">
          <h1 id='app1'>TODO<span id='app2'>App</span></h1>
         <input id='user' type="text" placeholder='Usuário' />
-        <input id='password' type="text" placeholder='Senha' />
+        <input id='password' type="password" placeholder='Senha' />
         <div id='login-div'>
           <button id='login'>Login</button>
           <button id='register'>Register</button>
